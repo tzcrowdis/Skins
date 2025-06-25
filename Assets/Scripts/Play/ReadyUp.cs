df73e4d98e87447fbf2b57daf8e588d8509c8676
tzcrowdis <100492586+tzcrowdis@users.noreply.github.com>
2025-06-24 19:48:18 -0600

    Ready Up Canvas

---
+++ Assets/Scripts/Play/ReadyUp.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ReadyUp : MonoBehaviour
+{
+    [Header("Ready Up")]
+    public GameObject readyUpPanel;
+    public GameObject skinBorder;
+    public Image skinImage;
+    public Button startButton;
+    bool skinSelected = false;
+    Skin selectedSkin;
+
+    [Header("Skin Selection")]
+    public GameObject skinSelectionScrollView;
+    public Transform skinSelectionContent;
+    public GameObject skinSelectionButtonPrefab;
+    List<Button> selectableSkins;
+
+    [Header("Collection")]
+    public Transform collectionContent;
+    
+
+    void Start()
+    {
+        startButton.onClick.AddListener(PlayOrSelectSkin);
+        if (skinSelected)
+            startButton.GetComponentInChildren<TMP_Text>().text = "Start";
+        else
+            startButton.GetComponentInChildren<TMP_Text>().text = "Equip Skin";
+
+        skinSelectionScrollView.SetActive(false);
+    }
+
+    void PlayOrSelectSkin()
+    {
+        if (skinSelected)
+        {
+            // TODO play the game
+
+            Home.instance.gameObject.SetActive(false);
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            UpdateSkinSelectionContent();
+            readyUpPanel.transform.position -= new Vector3(40f, 0f, 0f);
+            skinSelectionScrollView.SetActive(true);
+        }
+    }
+
+    void UpdateSkinSelectionContent()
+    {
+        foreach (Transform child in skinSelectionContent)
+            Destroy(child.gameObject);
+        
+        foreach (Transform child in collectionContent)
+        {
+            Skin skin = child.GetComponent<Skin>();
+            if (skin)
+            {
+                GameObject skinButton = Instantiate(skinSelectionButtonPrefab, skinSelectionContent);
+                skinButton.GetComponent<Button>().onClick.AddListener(delegate { SelectSkin(skin); });
+
+                Image img = skinButton.GetComponent<Image>();
+                img.sprite = skin.itemImage.sprite;
+                img.color = skin.itemImage.color;
+                img.material = skin.itemImage.material;
+            }
+        }
+    }
+
+    void SelectSkin(Skin skin)
+    {
+        selectedSkin = skin;
+        skinSelected = true;
+
+        skinImage.sprite = skin.itemImage.sprite;
+        skinImage.color = skin.itemImage.color;
+        skinImage.material = skin.itemImage.material;
+
+        startButton.GetComponentInChildren<TMP_Text>().text = "Start";
+        readyUpPanel.transform.position += new Vector3(40f, 0f, 0f);
+        skinSelectionScrollView.SetActive(false);
+    }
+
+    Skin GetSelectedSkin()
+    {
+        return selectedSkin;
+    }
+}
---
+++ Assets/Scripts/Store/CoinsOnSale.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinsOnSale : MonoBehaviour
+{
+    public Image coinImage;
+    public int coinAmount;
+    public int coinCost;
+}
